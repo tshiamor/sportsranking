6ebb2e7a65f8d01e8a20ed9aa6292f51abc88b9d
tshiamor@gmail.com <tshiamor@gmail.com>
2023-01-17 08:33:00 +0800

    add team result comparison

--- leaguerank/views.py
+++ leaguerank/views.py
@@ -10,6 +10,8 @@ from django.urls import reverse_lazy, reverse
 from .models import Leaguerank
 
 import pandas as pd
+import sqlite3
+from pathlib import Path
 
 class leaguerankBaseView(View):
     model = Leaguerank
@@ -36,6 +38,10 @@ class leaguerankDeleteView(leaguerankBaseView, DeleteView):
     """View to delete a leaguerank"""
 
 def upload_csv(request):
+    #conn =
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    db = BASE_DIR /'db.sqlite3'
+    conn = sqlite3.connect(db)
     if "GET" == request.method:
         return render(request, "myapp/upload_csv.html", data)
     elif "POST"== request.method:
@@ -44,16 +50,28 @@ def upload_csv(request):
         csv_file = request.FILES['myfile']
         #print(csv_file)
 
-        csv_payload = csv_file.read().decode("utf-8")
-        df = pd.read_csv(csv_payload,sep=',',delim_whitespace=True)
-        print(df)
+        #csv_payload = csv_file.read().decode("utf-8")
+        #df = pd.read_csv(csv_payload,sep=',',delim_whitespace=True)
+        df = pd.read_csv(csv_file, header=None)
+        #print(df)
+        matchResult = []
 
-        # lines = csv_payload.split("\n")
-        # for line in lines:
-        #     fields = line.split(",")
-        #     data_dict = {}
-		# 	data_dict["name"] = fields[0]
-		# 	data_dict["points"] = fields[1]
+        #print(df.shape)
+        for rows in df:
+            #print(rows)
+            print(df.loc[rows])
+            print('team1 score ->', df.loc[rows][3])
+            if(df.iloc[rows][3] > df.iloc[rows][1]):
+                matchResult.append({"team": df.iloc[rows][2], "points":  df.iloc[rows][3] })
+            else:
+                matchResult.append({"team": df.iloc[rows][0], "points":  df.iloc[rows][1] })
+        print(matchResult)
+
+        df2 = pd.DataFrame(matchResult)
+        print(df2)
+
+        df.to_sql('products', conn, if_exists='replace', index = False)
+
 
 
 
